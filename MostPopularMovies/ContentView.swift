45a8b973b44b5d589e252d35cc244d91a7d374c2
Isis Silva <isis.silva@arctouch.com>
2022-08-16 13:59:56 -0300

    Initial Commit

---
+++ MostPopularMovies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MostPopularMovies
+//
+//  Created by Isis Silva on 16/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MostPopularMovies/MostPopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostPopularMoviesApp.swift
+//  MostPopularMovies
+//
+//  Created by Isis Silva on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MostPopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
